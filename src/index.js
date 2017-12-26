a6e0fb00376d715ca37c2bc7be1e1d7f0a10f852
nik <fullcrimp@protonmail.com>
2017-12-26 10:57:02 +0300

    add brute force

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-module.exports = function splitAny(any) {
-    // your solution here
+function splitAny(input) {
+    switch (typeof input) {
+        case 'number':
+            return splitNumber(input)
+        break
+
+        case 'boolean':
+            throw TypeError()
+        break
+
+        case 'string':
+            return splitString(input)
+        break
+
+        case 'function':
+            return splitFunction(input)
+        break
+
+        case 'object':
+            if (Array.isArray(input)){
+                return splitArray(input) 
+            } else {
+                return splitObject(input)
+            }
+        break        
+    }
 }
+
+function splitString(str) {
+    return (str.indexOf(' ') === -1) 
+        ? str.split('') 
+        : str.split(' ').map((item) => splitString(item))
+}
+
+function splitNumber(num) {
+    return splitString(num.toString())
+        .map((item) => Number(item))
+}
+
+function splitFunction(fn) {
+    return splitAny(fn())
+}
+
+function splitArray(arr) {
+    return arr.map(item => splitAny(item))
+}
+
+function splitObject(obj) {
+    return Object.keys(obj).map(key => [key, obj[key]])
+}
+
+module.exports = splitAny;
