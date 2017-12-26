970bff424c5009b5b3948320360a9173fb1d94cb
nik <fullcrimp@protonmail.com>
2017-12-26 12:10:30 +0300

    make solution awesome

--- src/index.js
+++ src/index.js
@@ -1,52 +1,41 @@
-function splitAny(input) {
-    switch (typeof input) {
-        case 'number':
-            return splitNumber(input)
-        break
-
-        case 'boolean':
-            throw TypeError()
-        break
-
-        case 'string':
-            return splitString(input)
-        break
-
-        case 'function':
-            return splitFunction(input)
-        break
-
-        case 'object':
-            if (Array.isArray(input)){
-                return splitArray(input) 
-            } else {
-                return splitObject(input)
-            }
-        break        
-    }
-}
-
-function splitString(str) {
-    return (str.indexOf(' ') === -1) 
+let splitString = (str) => 
+    (str.indexOf(' ') === -1) 
         ? str.split('') 
-        : str.split(' ').map((item) => splitString(item))
-}
+        : str.split(' ')
+            .map((item) => splitString(item))
 
-function splitNumber(num) {
-    return splitString(num.toString())
+let splitNumber = (num) => 
+    splitString(num.toString())
         .map((item) => Number(item))
+
+let splitFunction = (fn) => 
+    splitAny(fn())
+
+let splitArray = (arr) => 
+    arr.map((item) => splitAny(item))
+
+let splitObjectLiteral = (obj) => 
+    Object.keys(obj)
+        .map((key) => [key, obj[key]])
+
+let splitObject = (obj) =>
+    Array.isArray(obj)
+        ? splitArray(obj) 
+        : splitObjectLiteral(obj)
+
+let splitBoolean = (val) => 
+    TypeError()
+
+
+let splitFn = {
+    'number': splitNumber,
+    'boolean': splitBoolean,
+    'string': splitString,
+    'function': splitFunction,
+    'object': splitObject
 }
 
-function splitFunction(fn) {
-    return splitAny(fn())
-}
+module.exports = splitAny = (input) => 
+    splitFn[typeof input](input)
 
-function splitArray(arr) {
-    return arr.map(item => splitAny(item))
-}
 
-function splitObject(obj) {
-    return Object.keys(obj).map(key => [key, obj[key]])
-}
-
-module.exports = splitAny;
